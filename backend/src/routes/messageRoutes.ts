bff01657e3a6f2a4587ddc2d1dbc699c5a5754b4
vini-2205 <viniciuspinheiro2003@gmail.com>
2025-01-10 21:04:15 -0300

    Atualização na edição de mensagens

--- backend/src/routes/messageRoutes.ts
+++ backend/src/routes/messageRoutes.ts
@@ -12,7 +12,9 @@ const upload = multer(uploadConfig);
 
 messageRoutes.get("/messages/:ticketId", isAuth, MessageController.index);
 messageRoutes.post("/messages/:ticketId", isAuth, upload.array("medias"), MessageController.store);
+messageRoutes.post("/messages/edit/:messageId", isAuth, upload.array("medias"), MessageController.edit);
 messageRoutes.delete("/messages/:messageId", isAuth, MessageController.remove);
 messageRoutes.post("/api/messages/send", tokenAuth, upload.array("medias"), MessageController.send);
+
 
 export default messageRoutes;
