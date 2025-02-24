7ed143aa829d6cf8c8b6c668a985f558ceff4486
vini-2205 <viniciuspinheiro2003@gmail.com>
2025-02-24 20:45:18 -0300

    Atualizacao página de login

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,11 +1,10 @@
 import React, { useState, useContext } from "react";
 import { Link as RouterLink } from "react-router-dom";
-
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid"; 
+import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
@@ -15,153 +14,133 @@ import { i18n } from "../../translate/i18n";
 import { nomeEmpresa } from "../../../package.json";
 import { AuthContext } from "../../context/Auth/AuthContext";
 import logo from "../../assets/logo.png";
+import Carousel from "../../components/Carousel"; // Importe o componente Carousel
 
-
-const Copyright = () => {
-	return (
-		<Typography variant="body2" color="primary" align="center">
-			{"Copyright "}
- 			<Link color="primary" href="#">
- 				{ nomeEmpresa } - v { versionSystem }
- 			</Link>{" "}
- 			{new Date().getFullYear()}
- 			{"."}
- 		</Typography>
- 	);
- };
-
-const useStyles = makeStyles(theme => ({
-	root: {
-		width: "100vw",
-		height: "100vh",
-		//background: "linear-gradient(to right, #682EE3 , #682EE3 , #682EE3)",
-		//backgroundImage: "url(https://i.imgur.com/CGby9tN.png)",
-		backgroundColor: theme.palette.primary.main,
-		backgroundRepeat: "no-repeat",
-		backgroundSize: "100% 100%",
-		backgroundPosition: "center",
-		display: "flex",
-		flexDirection: "column",
-		alignItems: "center",
-		justifyContent: "center",
-		textAlign: "center",
-	},
-	paper: {
-		backgroundColor: theme.palette.login,
-		display: "flex",
-		flexDirection: "column",
-		alignItems: "center",
-		padding: "55px 30px",
-		borderRadius: "12.5px",
-	},
-	avatar: {
-		margin: theme.spacing(1),  
-		backgroundColor: theme.palette.secondary.main,
-	},
-	form: {
-		width: "100%", // Fix IE 11 issue.
-		marginTop: theme.spacing(1),
-	},
-	submit: {
-		margin: theme.spacing(3, 0, 2),
-	},
-	powered: {
-		color: "white"
-	}
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100vw",
+    height: "100vh",
+    backgroundColor: theme.palette.primary.main,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container: {
+    display: "flex",
+    height: "85%",
+    width: "85%",
+    borderRadius: "12.5px",
+    overflow: "hidden",
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column", // Em telas pequenas, empilha
+      height: "80vh",
+      width: "90%",
+    },
+  },
+  adsContainer: {
+    flex: 1,
+    backgroundColor: "#f0f0f0",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: theme.spacing(2),
+    height: "100%",
+    overflow: "hidden",
+    [theme.breakpoints.down("sm")]: {
+      height: "70%", // O carrossel ocupa 50% da tela
+    },
+  },
+  loginContainer: {
+    flex: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: theme.spacing(4),
+    [theme.breakpoints.down("sm")]: {
+      height: "50%", // Reduz altura do login em telas pequenas
+    },
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
 }));
 
 const Login = () => {
-	const classes = useStyles();
+  const classes = useStyles();
+  const [user, setUser] = useState({ email: "", password: "" });
+  const { handleLogin } = useContext(AuthContext);
 
-	const [user, setUser] = useState({ email: "", password: "" });
+  const handleChangeInput = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
 
-	const { handleLogin } = useContext(AuthContext);
+  const handlSubmit = (e) => {
+    e.preventDefault();
+    handleLogin(user);
+  };
 
-	const handleChangeInput = e => {
-		setUser({ ...user, [e.target.name]: e.target.value });
-	};
-
-	const handlSubmit = e => {
-		e.preventDefault();
-		handleLogin(user);
-	};
-
-	
-	return (
-		<div className={classes.root}>
-		<Container component="main" maxWidth="xs">
-			<CssBaseline/>
-			<div className={classes.paper}>
-				<div>
-					<img style={{ margin: "0 auto", width: "70%" }} src={logo} alt="Whats" />
-				</div>
-				{/*<Typography component="h1" variant="h5">
-					{i18n.t("login.title")}
-				</Typography>*/}
-				<form className={classes.form} noValidate onSubmit={handlSubmit}>
-					<TextField
-						variant="outlined"
-						margin="normal"
-						required
-						fullWidth
-						id="email"
-						label={i18n.t("login.form.email")}
-						name="email"
-						value={user.email}
-						onChange={handleChangeInput}
-						autoComplete="email"
-						autoFocus
-					/>
-					<TextField
-						variant="outlined"
-						margin="normal"
-						required
-						fullWidth
-						name="password"
-						label={i18n.t("login.form.password")}
-						type="password"
-						id="password"
-						value={user.password}
-						onChange={handleChangeInput}
-						autoComplete="current-password"
-					/>
-					
-					{/* <Grid container justify="flex-end">
-					  <Grid item xs={6} style={{ textAlign: "right" }}>
-						<Link component={RouterLink} to="/forgetpsw" variant="body2">
-						  Esqueceu sua senha?
-						</Link>
-					  </Grid>
-					</Grid>*/}
-					
-					<Button
-						type="submit"
-						fullWidth
-						variant="contained"
-						color="primary"
-						className={classes.submit}
-					>
-						{i18n.t("login.buttons.submit")}
-					</Button>
-					{ <Grid container>
-						<Grid item>
-							<Link
-								href="#"
-								variant="body2"
-								component={RouterLink}
-								to="/signup"
-							>
-								{i18n.t("login.buttons.register")}
-							</Link>
-						</Grid>
-					</Grid> }
-				</form>
-			
-			</div>
-			<Box mt={8}><Copyright /></Box>
-		</Container>
-		</div>
-	);
+  return (
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <div className={classes.adsContainer}>
+          <Carousel />
+        </div>
+        <div className={classes.loginContainer}>
+          <img style={{ margin: "0 auto", width: "70%" }} src={logo} alt="Logo" />
+          <form className={classes.form} noValidate onSubmit={handlSubmit}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label={i18n.t("login.form.email")}
+              name="email"
+              value={user.email}
+              onChange={handleChangeInput}
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label={i18n.t("login.form.password")}
+              type="password"
+              id="password"
+              value={user.password}
+              onChange={handleChangeInput}
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              {i18n.t("login.buttons.submit")}
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href="#" variant="body2" component={RouterLink} to="/signup">
+                  {i18n.t("login.buttons.register")}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
